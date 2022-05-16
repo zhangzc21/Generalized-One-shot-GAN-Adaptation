4dace44e2527c5fb24b0809fb439811c307fbfb4
thevoidname <105484386+thevoidname@users.noreply.github.com>
2022-05-16 12:39:52 +0800

    Add files via upload

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -3,8 +3,8 @@
 adapted_model='adapted_models/disney.pkl' # path to adapted model
 ############################################
 ##############alternative###################
-test_image='alternative' # path or dir to the real images that will be inverted by e4e
-out_dir='alternative'  # save dir
+test_image='test_data/real_images' # path or dir to the real images that will be inverted by e4e
+out_dir='None'  # save dir
 ############################################
 
 python inference.py \
@@ -12,4 +12,5 @@ python inference.py \
 --out_dir $out_dir \
 --adapted_model $adapted_model \
 --pretrained_model pretrained_models/ffhq.pkl \
---e4e_model pretrained_models/e4e_ffhq_encode.pt
+--e4e_model pretrained_models/e4e_ffhq_encode.pt \
+--show True
