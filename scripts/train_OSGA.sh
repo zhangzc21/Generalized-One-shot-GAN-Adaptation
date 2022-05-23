1b6042ef8ba0920d67383856c399a3c6a890db65
thevoidname <105484386+thevoidname@users.noreply.github.com>
2022-05-23 15:52:08 +0800

    Update train_OSGA.sh

--- scripts/train_OSGA.sh
+++ scripts/train_OSGA.sh
@@ -1,25 +1,29 @@
 ############################################
 ##############MUST BE DEFINED###############
-image_path="test_data/references/disney.png" # path to the reference image
+image_path="test_data/references_ent/hat.png" # path to the reference image
 out_dir="Results" # dir to save results
+mask_dir="test_data/masks" # all entity masks should be placed in the mask_dir
 ############################################
-#############alternative###################
+#############optional###################
 test_image_path="test_data/real_images"
 ############################################
 
-python one_shot_style_adaption.py --exp_name 0522 \
+python generalized_one_shot_adaption.py --exp_name  test \
 --image_path $image_path \
 --out_dir $out_dir \
+--mask_dir $mask_dir \
 --test_image_path $test_image_path \
---pretrained_model pretrained_models/ffhq.pkl \
+--pretrained_model "pretrained_models/ffhq.pkl" \
 --source_domain face \
---e4e_model pretrained_models/e4e_ffhq_encode.pt \
 --index 8 \
---flip_aug True \
---total_step 1000 \
+--total_step 2000 \
 --batch 1 \
---reg_weight 0.5 \
---lpips_weight 10 \
---style_weight 2 \
---vgg_feature_num 3 4 \
 --fix_style True \
+--lpips_weight 10 \
+--entity_weight 2 \
+--reg_weight 1 \
+--style_weight 0.2 \
+--vgg_feature_num 3 4 \
+--use_mask True \
+--flip_aug True \
+--e4e_model pretrained_models/e4e_ffhq_encode.pt #comment this line if you want to invert image with mask
