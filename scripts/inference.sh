e8435c9e74b535805649ef3cdb922f7260312c96
thevoidname <105484386+thevoidname@users.noreply.github.com>
2022-05-23 16:03:10 +0800

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,8 +1,8 @@
 ############################################
 ##############MUST BE DEFINED###############
-adapted_model='adapted_models/disney.pkl' # path to adapted model
+adapted_model='adapted_models/glass.pkl' # path to adapted model
 ############################################
-##############alternative###################
+##############optional###################
 test_image='test_data/real_images' # path or dir to the real images that will be inverted by e4e
 out_dir='None'  # save dir
 ############################################
